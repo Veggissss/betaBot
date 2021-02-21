8ddeaca6a3a1cfd805ec1423e390e0be31fe27d1
Vegard Demring <vegarddem@gmail.com>
2021-02-21 23:37:13 +0100

    1.1.0

--- embed.js
+++ embed.js
@@ -2,10 +2,14 @@ const Discord = require('discord.js')
 const fs = require('fs');
 
 //Sends and updates leaderboard and gives out ranks
-function sendEmbed(discordClient = new Discord.Client(),ChannelID,messageID,message2ID){
+function sendEmbed(discordClient = new Discord.Client(), GuildID, ChannelID){
     
     const statsChannel = discordClient.channels.cache.get(ChannelID);
-    const guild = discordClient.guilds.cache.get("451365873668849664");
+    const guild = discordClient.guilds.cache.get(GuildID);
+
+    const embedID = fs.readFileSync("embed.json");
+    const messageID = JSON.parse(embedID); 
+    const serverIcon = guild.iconURL();
   
     // Read users.json file 
     fs.readFile("users.json", function(err, data) { 
@@ -16,34 +20,39 @@ function sendEmbed(discordClient = new Discord.Client(),ChannelID,messageID,mess
         // Converting to JSON 
         const users = JSON.parse(data); 
 
-        //Sort by voiceTime
+        //Sort by score
         users.sort(GetSortOrder("score"));
 
         //Give top user rank
-        let role = guild.roles.cache.find(role => role.id === "711141574964412416");
-        GiveTopRole(guild,users[0].userID, role);
-        //console.log(`Gave rank ${role.name} to ${users[0].username}`);
+        const topData = fs.readFileSync("top.json"); 
+        const topRank = JSON.parse(topData); 
+
+        if (topRank.userID != users[0].userID){
+            let role = guild.roles.cache.find(role => role.id === "711141574964412416");
+
+            RemoveRole(guild,topRank.userID,role);
+            GiveRole(guild,users[0].userID, role);
+            console.log(`Removed toprank from ${topRank.username} and gave it to ${users[0].username}`);
+
+            fs.writeFileSync("top.json", JSON.stringify(users[0], null, 2), err => { if (err) throw err; });
+        }
         
         let userScore='';
         let userNames = '';
         let userMsgTime = '';
 
-        //console.log(users);
-        
-        for (let i = 0; i < users.length; i++) {
+        for (var i = 0; i < users.length; i++) {
             var score = users[i].score;
             var username = users[i].username;
             var messages = users[i].messages;
             var voiceTime= users[i].voiceTime;
 
-            var voiceMins= voiceTime/1000/60;
-            var voiceHour= Math.round((voiceMins/60)*10)/10;
+            let voiceMins= voiceTime/1000/60;
+            let voiceHour= Math.round((voiceMins/60)*10)/10;
 
             userNames += `\`${i + 1}\` ${username}\n`;
             userMsgTime += `\`${messages} messages / ${voiceHour} hours\`\n`;
             userScore+= `\`${score}\`\n`;
-
-            var score = users[i].score;
 
             //Role rewards (delta,mafia,trusted,foregeiner) + DJ
             if (score>10000){
@@ -63,42 +72,63 @@ function sendEmbed(discordClient = new Discord.Client(),ChannelID,messageID,mess
             }
 
             let role = guild.roles.cache.find(role => role.id === rank);
-            GiveTopRole(guild,users[i].userID, role);
+            GiveRole(guild,users[i].userID, role);
             //console.log(`Gave rank ${role.name} to ${users[i].username}`);
         }
 
         const leaderboard = new Discord.MessageEmbed()
-        //.setAuthor(`Leaderboard for Narkos (Started 08.02.2021)`, message.guild.iconURL({ dynamic: true }))
-        .setAuthor(`Leaderboard for Narkos`)
-        .setColor('#0099ff') //.setColor(0x51267)
-        .addFields(
-            { name: 'Username',    value: userNames, inline: true },
-            { name: 'Stats',  value: userMsgTime, inline: true },
-            { name: 'Score',     value: userScore, inline: true })
-        .setFooter('Score:  5 points per message  /  30 points per hour');
+            .setAuthor(`Leaderboard for Narkos   /   ${guild.memberCount} members`)    //servericon or ,discordClient.user.avatarURL()
+            .setColor('#0099ff') //.setColor(0x51267)
+            .addFields(
+                { name: 'Username', value: userNames,   inline: true },
+                { name: 'Stats',    value: userMsgTime, inline: true },
+                { name: 'Score',    value: userScore,   inline: true })
+            .setFooter('Score:  5 points per message  /  30 points per hour');
         
-        //statsChannel.send(leaderboard);
-        statsChannel.messages.fetch({around: messageID, limit: 1})
-        .then(msg => {
+        //Edit message, send it otherwise
+        
+        statsChannel.messages.fetch({around: messageID.leaderboard, limit: 1}).then(msg => {
             const fetchedMsg = msg.first();
-            fetchedMsg.edit(leaderboard);
+
+            if (fetchedMsg != undefined){
+                fetchedMsg.edit(leaderboard);
+            }
+            else{
+                console.log("Scoreboard embed found");
+                statsChannel.send(leaderboard).then(sent => {
+                    messageID.leaderboard = sent.id;
+
+                    fs.writeFileSync("embed.json", JSON.stringify(messageID, null, 2), err => { if (err) throw err; });
+                });
+            }
         });
 
+
         const rewards = new Discord.MessageEmbed()
-        //.setAuthor(`Leaderboard for Narkos (Started 08.02.2021)`, message.guild.iconURL({ dynamic: true }))
-        .setAuthor(`Rewards for Narkos`)
-        .setColor('#0099ff')
-        .setThumbnail('https://i.imgur.com/5r6uISG.png')
-        .addFields(
-            { name: 'Reward',           value: `<@&${"711141574964412416"}>\n<@&${"491506230355951636"}>\n<@&${"693894552179834891"}>\n<@&${"491151062019997696"}>\n<@&${"641358849865154581"}>`, inline: true },
-            { name: 'Required Score',   value: "Rank 1\n10 000\n5 000\n2 000\n500", inline: true })
-        .setFooter('Updates on vc join!');
+            .setAuthor(`Rewards for Narkos`)
+            .setColor('#0099ff')
+            .setThumbnail(serverIcon)
+            .addFields(
+                { name: 'Reward',           value: `<@&${"711141574964412416"}>\n<@&${"491506230355951636"}>\n<@&${"693894552179834891"}>\n<@&${"491151062019997696"}>\n<@&${"641358849865154581"}>`, inline: true },
+                { name: 'Required Score',   value: "Rank 1\n10 000\n5 000\n2 000\n500", inline: true })
+            .setFooter('Updates when user leaves vc!');
         
-        //statsChannel.send(rewards);
-        statsChannel.messages.fetch({around: message2ID, limit: 1})
+        
+        statsChannel.messages.fetch({around: messageID.rewards, limit: 1})
         .then(msg => {
             const fetchedMsg = msg.first();
-            fetchedMsg.edit(rewards);
+
+            if (fetchedMsg != undefined){
+                fetchedMsg.edit(rewards);
+            }
+            else{
+                console.log("Rewards embed not found");
+                statsChannel.send(rewards).then(sent => {
+                    messageID.rewards = sent.id;
+        
+                    fs.writeFileSync("embed.json", JSON.stringify(messageID, null, 2), err => { if (err) throw err; });
+                });
+            }
         });
     })
 }
@@ -114,11 +144,26 @@ function GetSortOrder(prop) {
         }    
         return 0;    
     }    
-}  
+}
 
 //Give role rewards
-async function GiveTopRole(guild,userID, role){
-    (await guild.members.fetch(userID)).roles.add(role);
+async function GiveRole(guild,userID,role){
+    try{
+        (await guild.members.fetch(userID)).roles.add(role);
+    }
+    catch (error){
+        console.log(`Can't give role to member, member not found.\nUser ID: ${userID}`);
+    }
+}
+
+//Give role rewards
+async function RemoveRole(guild,userID,role){
+    try{
+        (await guild.members.fetch(userID)).roles.remove(role);
+    }
+    catch (error){
+        console.log(`Can't remove role to member.\nUser ID: ${userID}`);
+    }
 }
 
 
--- index.js
+++ index.js
@@ -2,26 +2,25 @@
 //StatBot started 08.02.2021
 //Calculates time spent in vc and the amount messages sendt per user
 
-//Bug if a user is in afk and joins a normal vc and then leaves then they will get afk time as well :P
-
-//Import libraries:
+//Import local files
 const config = require('./settings.json');
 const embed  = require('./embed.js');
 
-//Remember u need npm i opusscript to play sounds!
-const Discord = require('discord.js');
+const Discord = require('discord.js');                              //Remember you need "npm i opusscript" to play sounds!
 const fs = require('fs');
 
 //Create discord client
-const discordClient = new Discord.Client(); //{partials: ["MESSAGE","CHANNEL","REACTION","USER","GUILD_MEMBER"]}
+const discordClient = new Discord.Client();                         //{partials: ["MESSAGE","CHANNEL","REACTION","USER","GUILD_MEMBER"]}
 
-//Arrays
-const afkChannels = ["451371568577249281","808276679389610005"];
+//Global Variables
+const discordServerID = "451365873668849664";                       //Discord server ID
+const discordChannelID = "808649489307926529";                      //Channel where scoreboard should be posted
+const afkChannels = ["451371568577249281","808276679389610005"];    //Channels that gives 0 points
 
 //discord startup:
 discordClient.on('ready', () => {
   console.log(`Logged in as ${discordClient.user.tag}!\n`)
-  discordClient.user.setActivity("with fire!", {type: "PLAYING"}); //LISTENING //PLAYING
+  discordClient.user.setActivity("with fire!", {type: "PLAYING"});  //LISTENING //PLAYING
 })
 
 //Direct Messages:
@@ -43,8 +42,7 @@ discordClient.on('message', (message) => {
         // Converting to JSON 
         const users = JSON.parse(data); 
 
-        let id = message.author.id;
-        let search = searchID(users,id);
+        let search = searchID(users, message.author.id);
         let userFound = search[0], n = search[1];
 
         //console.log(userFound,n);
@@ -61,6 +59,9 @@ discordClient.on('message', (message) => {
             console.log(`${message.author.username} has sent ${entry.messages} messages`);
 
             fs.writeFileSync("users.json", JSON.stringify(users,null,2), err => { if (err) throw err;  });
+
+            //Update leaderboard embed
+            embed.sendEmbed(discordClient, discordServerID, discordChannelID);
         }
         else{
             // Defining new user 
@@ -76,11 +77,14 @@ discordClient.on('message', (message) => {
             //Add new user to json
             users.push(user);
             fs.writeFileSync("users.json", JSON.stringify(users,null,2), err => { if (err) throw err; });
+
+            //Update leaderboard embed
+            embed.sendEmbed(discordClient, discordServerID, discordChannelID);
         }
     })
   }
 })
-    
+
 
 //Triggered when user joins/leaves mutes/unmutes. The lather really sucks...
 discordClient.on("voiceStateUpdate", (oldMember, newMember)=> { 
@@ -101,19 +105,10 @@ discordClient.on("voiceStateUpdate", (oldMember, newMember)=> {
 
     //User Joins
     if (oldVoice == null) {
-        //Update leaderboard embed
-        embed.sendEmbed(discordClient,"808649489307926529","808649811788038165","808649813703917578");
-
         console.log(`${username} joined!`);
-        //Make afktime 0 here
-        
+
         var date = new Date();
-        var msec = date.getTime();
-
-        var joinTime = msec;
-
-        //TODO Save jointime in json
-        var id = oldMember.member.id;
+        var joinTime = date.getTime();
 
         // Read users.json file 
         fs.readFile("users.json", function(err, data) { 
@@ -124,8 +119,9 @@ discordClient.on("voiceStateUpdate", (oldMember, newMember)=> {
             // Converting to JSON 
             const users = JSON.parse(data); 
 
-            let search = searchID(users,id);
-            var userFound = search[0], n = search[1];
+            let search = searchID(users, oldMember.member.id);
+            let userFound = search[0]
+            let n = search[1];
 
             //console.log(userFound,n);
             if (userFound){
@@ -140,12 +136,6 @@ discordClient.on("voiceStateUpdate", (oldMember, newMember)=> {
                 }
 
                 entry.voiceJoin = joinTime;
-
-                //5 points for message, 30 points per hour in vc
-                var calculation = (5 * entry.messages + (30*(entry.voiceTime /1000/60/60)))
-                var newScore = Math.round(calculation)
-                
-                entry.score = newScore;
 
                 fs.writeFileSync("users.json", JSON.stringify(users,null,2), err => { if (err) throw err;  });
             }else{
@@ -171,21 +161,17 @@ discordClient.on("voiceStateUpdate", (oldMember, newMember)=> {
         console.log(`${username} left!\n`);
 
         var date = new Date();
-        var msec = date.getTime();
+        var leaveTime = date.getTime();
 
-        var leaveTime = msec;
-
-        var id = oldMember.member.id;
-        
         // Read users.json file 
         fs.readFile("users.json", function(err, data) { 
             if (err) throw err; 
         
-            // Converting to JSON 
+            // Converting to JSON array
             const users = JSON.parse(data); 
 
             //See if userID is in json
-            let search = searchID(users,id);
+            let search = searchID(users, oldMember.member.id);
 
             //Boolean and an array index
             var userFound = search[0], n = search[1];
@@ -209,7 +195,17 @@ discordClient.on("voiceStateUpdate", (oldMember, newMember)=> {
 
                 entry.voiceTime += time;
 
+                //5 points for message, 30 points per hour in vc
+                let calculation = (5 * entry.messages + (30*(entry.voiceTime /1000/60/60)))
+                let newScore = Math.round(calculation)
+                
+                entry.score = newScore;
+
                 fs.writeFileSync("users.json", JSON.stringify(users,null,2), err => { if (err) throw err; });
+
+                //Update leaderboard embed
+                embed.sendEmbed(discordClient, discordServerID, discordChannelID);
+
             } else {
                 console.log("User not found.");
             }
@@ -219,11 +215,10 @@ discordClient.on("voiceStateUpdate", (oldMember, newMember)=> {
     //User switches voice channel
     else {
         console.log(`${username} switched channels!`);
+
         if (afkChannels.includes(oldMember.channelID) || (afkChannels.includes(newMember.channelID))){
             console.log(`${oldMember.member.displayName} is afk`);
 
-            var id = oldMember.member.id;
-        
             // Read users.json file 
             fs.readFile("users.json", function(err, data) { 
                 if (err) throw err; 
@@ -232,7 +227,7 @@ discordClient.on("voiceStateUpdate", (oldMember, newMember)=> {
                 const users = JSON.parse(data); 
 
                 //See if userID is in json
-                let search = searchID(users,id);
+                let search = searchID(users, oldMember.member.id);
 
                 //Boolean and an array index
                 var userFound = search[0], n = search[1];
